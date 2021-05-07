dcfda6365d6272441ab3c99c34061f518809260e
John Katieno <katienoj@gmail.com>
2021-05-07 11:35:10 +0300

    Initial configuration

--- Angular/Models/user.js
+++ Angular/Models/user.js
@@ -1,30 +1,33 @@
-//create new model
-var mongoose = require('mongoose'); //refering mongoose for creating user friendly class type model.
-//defining schema for user model
+var mongoose = require('mongoose');
+
 var userSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    email: String,
-    role: String,
-    created_at: {type: Date, default: Date.now}
-    
+	username: String,
+	password: String,
+	email: String,
+	role: String,
+	created_at: {type: Date, default: Date.now}
+	
 });
+
 mongoose.model('User', userSchema);
 var User = mongoose.model('User');
 exports.findByUsername = function(userName, callback){
-User.findOne({ user_name: userName}, function(err, user){
-        if(err){
-            return callback(err);
-        }
-return callback(null, user);
-});
+
+	User.findOne({ user_name: userName}, function(err, user){
+		if(err){
+			return callback(err);
+		}
+		//success
+		return callback(null, user);
+	});
 }
 
 exports.findById = function(id, callback){
-User.findById(id, function(err, user){
-        if(err){
-           return callback(err);
-        }
-         return callback(null, user);
-    });
+	User.findById(id, function(err, user){
+		if(err){
+			return callback(err);
+		}
+		return callback(null, user);
+	});
 }
+
